6ee245f6801a754d514e80d0c9d8ce8fff7f0c54
DanZhun <dan861202@gmail.com>
2022-06-16 03:52:44 +0800

    demo01

---
+++ js/add-contents.js
@@ -0,0 +1,9 @@
+//Date floor
+$(document).ready(function() {
+
+    let today = new Date();
+    let year = today.getFullYear();
+   
+     $('#footer').html('<p>Copyright &copy;' + year + ' ' );
+   
+   });
